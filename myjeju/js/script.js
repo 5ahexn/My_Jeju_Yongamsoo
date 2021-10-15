c88dc1c3c64995d47e9608e89aeaae7480b98816
5ahexn <90259048+5ahexn@users.noreply.github.com>
2021-10-15 20:06:11 +0900

    Add files via upload

---
+++ myjeju/js/script.js
@@ -0,0 +1,20 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+const navi = document.querySelector('.right-menu');
+const menuBtn = document.querySelector('.menu-btn');
+
+menuBtn.addEventListener('click', () => {
+    navi.classList.toggle('gnb-btn');
+});
